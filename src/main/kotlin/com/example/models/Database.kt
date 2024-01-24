1298a4e9339ce22b86829892ad561a0633047084
arthraw <arthursallesandrade@gmail.com>
2024-01-24 00:50:28 -0300

    feat: Database driver changed

--- src/main/kotlin/com/example/models/Database.kt
+++ src/main/kotlin/com/example/models/Database.kt
@@ -1,5 +1,6 @@
 package com.example.models
 
+import io.github.cdimascio.dotenv.dotenv
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
@@ -8,9 +9,15 @@ import org.jetbrains.exposed.sql.transactions.transaction
 
 object DatabaseSingleton {
     fun init() {
-        val driverClassName = "org.h2.Driver"
-        val jdbcURL = "jdbc:h2:file:./build/db"
-        val database = Database.connect(jdbcURL, driverClassName)
+        val dotenv = dotenv()
+
+        val dbUser = dotenv.get("DATABASE_USER")
+        val dbPass = dotenv.get("DATABASE_PASSWORD")
+
+        val driverClassName = "com.mysql.cj.jdbc.Driver"
+        val jdbcURL = "jdbc:mysql://localhost:3306/posto"
+
+        val database = Database.connect(jdbcURL, driver = driverClassName, user = dbUser, password = dbPass)
         transaction(database) {
             SchemaUtils.create(Transactions)
         }
